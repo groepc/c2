ed527eb857c62518d997a026f01e3370c5bfebed
Perry Faro <perryfaro@live.nl>
2015-05-07 17:52:12 +0200

    Merge branch 'master' of github.com:groepc/c2
    Conflicts:
    	errorlog.html

--- app/controllers/tentamen.php
+++ app/controllers/tentamen.php
@@ -7,43 +7,50 @@ use models\Authenticate\Authenticate;
 use models\Tentamen\Tentamen as tentamentje;
 
 class Tentamen extends \core\controller {
-		
-	/**
-	* Call the parent construct
-	*/
-	public function __construct() {
-		Authenticate::isAuthenticated();
-		parent::__construct();
-	}
-		
-	/**
-	* Define Index page title and load template files
-	*/
-	public function index($type = false, $id = false) {
-		$data['title'] = 'Tentamen overzicht';
-		
-		$tentamen = new tentamentje;
-		$data['error'] = '';
 
-		if ($type !== false && $id !== false) {
+    /**
+     * Call the parent construct
+     */
+    public function __construct() {
+        Authenticate::isAuthenticated();
+        parent::__construct();
+    }
 
-			if ($type == 'in') {
-				$tentamen->schrijfIn(\helpers\session::get('userID'), $id);
-				
-			} elseif ($type == 'uit') {					
-				$tentamen->schrijfUit(\helpers\session::get('userID'), $id);
-				
-			} else {
-				$data['error'] = 'Er is iets fout gegaan, probeer opnieuw.';
-			}
-			\helpers\url::redirect('tentamens');
-		}
-		
-		$data['tentamen'] = $tentamen->getTentamens();
-		$data['inschrijving'] = $tentamen->getInschrijvingen(\helpers\session::get('userID'));
-		
-		View::rendertemplate('header', $data);
-		View::render('tentamen/index', $data);
-		View::rendertemplate('footer', $data);
-	}
+    /**
+     * Define Index page title and load template files
+     */
+    public function index($type = false, $id = false) {
+        $data['title'] = 'Tentamen overzicht';
+
+        $tentamen = new tentamentje;
+        $data['error'] = '';
+
+        if ($type !== false && $id !== false) {
+
+            if ($type == 'in') {
+                $result = $tentamen->schrijfIn(\helpers\session::get('userID'), $id);
+                if ($result === false) {
+                    \helpers\session::set('error', 'Je kunt je niet inschrijven voor dit tentamen omdat je al bent ingeschreven voor het zelfde tentamen of op de zelfde tijd');
+                }
+                \helpers\url::redirect('tentamens');
+            } elseif ($type == 'uit') {
+                $tentamen->schrijfUit(\helpers\session::get('userID'), $id);
+                \helpers\url::redirect('tentamens');
+            } else {
+                $data['error'] = 'Er is iets fout gegaan, probeer opnieuw.';
+            }
+        }
+
+        if (!empty(\helpers\Session::get('error'))) {
+            $data['error'] = \helpers\Session::pull('error');
+        }
+
+        $data['tentamen'] = $tentamen->getTentamens();
+        $data['inschrijving'] = $tentamen->getInschrijvingen(\helpers\session::get('userID'));
+
+        View::rendertemplate('header', $data);
+        View::render('tentamen/index', $data);
+        View::rendertemplate('footer', $data);
+    }
+
 }
--- app/models/Tentamen/Tentamen.php
+++ app/models/Tentamen/Tentamen.php
@@ -4,60 +4,67 @@ namespace models\Tentamen;
 
 class Tentamen extends \core\model {
 
-	/**
-		* Haal de tentamenlijst op.
-		* 
-		* @return array tentamenlijst
-	*/
-	public function getTentamens() {
+    /**
+     * Haal de tentamenlijst op.
+     * 
+     * @return array tentamenlijst
+     */
+    public function getTentamens() {
 
-		$array = $this->_db->select('SELECT *, '
-			. "DATE_FORMAT(planning.datumtijd, '%d-%m-%Y %H:%i') datumeu "
-			. 'FROM planning ');
-		return $array;
-	}
+        $array = $this->_db->select('SELECT *, '
+                . "DATE_FORMAT(planning.datumtijd, '%d-%m-%Y %H:%i') datumeu "
+                . 'FROM planning ORDER BY planning.datumtijd ASC');
+        return $array;
+    }
 
-	/**
-		* Haal de lijst met inschijvingen vanmde gebruiker op.
-		* 
-		* @return array lijst van inschrijvingen van de gebruiker
-	*/
-	public function getInschrijvingen($userID) {
-	
-		$array = $this->_db->select('SELECT * '
-			. 'FROM inschrijving '
-			. 'WHERE gebruikerID=:userID ', array(':userID' => $userID));
-		return $array;
-	}
-	
-	/**
-		* Schrijf de gebruiker in voor een tentamen
-	*/
-	
-	public function schrijfIn($userID, $tentamenID) {
-		
-		$data = array(			
-			'planningID' => $tentamenID,
-			'gebruikerID' => $userID,
-			'datumtijd' => date('Y-m-d H:i:s')			
-		);
-		
-		$array = $this->_db->insert('inschrijving', $data);
+    /**
+     * Haal de lijst met inschijvingen vanmde gebruiker op.
+     * 
+     * @return array lijst van inschrijvingen van de gebruiker
+     */
+    public function getInschrijvingen($userID) {
 
-	}
+        $array = $this->_db->select('SELECT * '
+                . 'FROM inschrijving '
+                . 'WHERE gebruikerID=:userID ', array(':userID' => $userID));
+        return $array;
+    }
 
-	/**
-		* Schrijf de gebruiker uit voor een tentamen
-	*/
-	
-	public function schrijfUit($userID, $tentamenID) {
+    /**
+     * Schrijf de gebruiker in voor een tentamen
+     */
+    public function schrijfIn($userID, $tentamenID) {
 
-		$data = array(			
-			'planningID' => $tentamenID,
-			'gebruikerID' => $userID			
-		);
+        $array = $this->_db->select('SELECT datumtijd FROM planning WHERE ID=:ID ', array(':ID' => $tentamenID));
+        $datumtijd = $array[0]->datumtijd;
 
-		$array = $this->_db->delete('inschrijving', $data);
+        $result = $this->_db->select('SELECT * FROM inschrijving JOIN planning ON (planning.ID=inschrijving.planningID) WHERE :datumtijd BETWEEN planning.datumtijd AND  DATE_ADD(planning.datumtijd, INTERVAL 2 HOUR) AND gebruikerId=:userID', array(':userID' => $userID, ':datumtijd' => $datumtijd));
 
-	}
+        if (!empty($result)) {
+            return false;
+        }
+
+        $data = array(
+            'planningID' => $tentamenID,
+            'gebruikerID' => $userID,
+            'datumtijd' => date('Y-m-d H:i:s')
+        );
+
+        $array = $this->_db->insert('inschrijving', $data);
+        return true;
+    }
+
+    /**
+     * Schrijf de gebruiker uit voor een tentamen
+     */
+    public function schrijfUit($userID, $tentamenID) {
+
+        $data = array(
+            'planningID' => $tentamenID,
+            'gebruikerID' => $userID
+        );
+
+        $array = $this->_db->delete('inschrijving', $data);
+    }
+
 }
